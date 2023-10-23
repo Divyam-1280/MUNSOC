61807290322b54985dbd8e998c47071ad06d9261
Divyam-1280 <divyamraj700@gmail.com>
2023-10-23 12:28:43 +0530

    caraousel done for now

--- MUNSOC/src/components/Carousel.jsx
+++ MUNSOC/src/components/Carousel.jsx
@@ -2,17 +2,21 @@ import React from 'react'
 import ScrollCarousel from 'scroll-carousel';
 import CarouselCard from './CarouselCard';
 import Mouni from './../assets/Mouni.jpeg'
-new ScrollCarousel(".my-carousel", {speed: 8, smartSpeed: true, autoplay: true});
+import Chai from './../assets/Chai.jpg'
+import Varun from './../assets/varun.jpeg'
+import Divyam from './../assets/divyam.jpeg'
+import Priyanshu from './../assets/priyanshu.jpeg'
+import Ananya from './../assets/ananya.jpeg'
 
 function Carousel() {
   return (
     <div className="mt-40 mb-40 my-carousel overflow-x-scroll flex">
-        <CarouselCard/>
-        <CarouselCard/>
-        <CarouselCard/>
-        <CarouselCard/>
-        <CarouselCard/>
-        <CarouselCard/>
+        <CarouselCard Img={Mouni} Name={"Mouni Sanapala"} Position={"President"} />
+        <CarouselCard Img={Chai} Name={"Chaitanya Raj"} Position={"Secretary General"}/>
+        <CarouselCard Img={Varun} Name={"Varun Srivastava"} Position={"Charge D' Affairs"}/>
+        <CarouselCard Img={Divyam} Name={"Divyam Raj Singh"} Position={"Under Secretary"}/>
+        <CarouselCard Img={Priyanshu} Name={"Priyanshu Das"} Position={"Under Secretary"}/>
+        <CarouselCard Img={Ananya} Name={"Ananya Shankar"} Position={"Treasurer"}/>
         
     </div>
     
