04e790fc9f5deaab68b82b0f4e8bdcf6ca823574
Divyam-1280 <divyamraj700@gmail.com>
2023-10-23 11:42:41 +0530

    carousel fixing to start now

--- MUNSOC/src/components/Carousel.jsx
+++ MUNSOC/src/components/Carousel.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import ScrollCarousel from 'scroll-carousel';
 import CarouselCard from './CarouselCard';
+import Mouni from './../assets/Mouni.jpeg'
 new ScrollCarousel(".my-carousel", {speed: 8, smartSpeed: true, autoplay: true});
 
 function Carousel() {
   return (
-    <div className="mt-40 h-72 mb-40 w-screen my-carousel flex gap-4">
+    <div className="mt-40 mb-40 my-carousel overflow-x-scroll flex">
         <CarouselCard/>
         <CarouselCard/>
         <CarouselCard/>
         <CarouselCard/>
         <CarouselCard/>
         <CarouselCard/>
+        
     </div>
     
   )
--- MUNSOC/src/components/HouseSection.jsx
+++ MUNSOC/src/components/HouseSection.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function HouseSection() {
   return (
     <div className=''>
-        <div className="text-white lg:text-4xl text-3xl w-screen flex justify-center items-center house-head">
+        <div className="text-white lg:text-4xl text-3xl w-full flex justify-center items-center house-head">
             House System
         </div>
     </div>
