5c62cf970f537f2bb75a3240ae79d439c3513f2a
Divyam-1280 <divyamraj700@gmail.com>
2023-10-26 20:47:09 +0530

    scroll fix try #2

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Contact from './components/Contact'
 function App() {
 
   return (
-    <div>
+    <div className="total-wrapper">
       <video src={Bgvid} autoPlay loop playsInline muted className="h-screen w-full object-cover" />
       <div className=" top-0 bottom-0 left-0 right-0 bg-black/60 absolute z-0 h-screen"></div>
       <section className="h-full flex flex-col">
