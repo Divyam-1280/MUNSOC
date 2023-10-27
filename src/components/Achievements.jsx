1671e0bb156fd3bbb9c570a715845481bdeabe3d
Divyam-1280 <divyamraj700@gmail.com>
2023-10-27 14:46:13 +0530

    content update

--- src/components/Achievements.jsx
+++ src/components/Achievements.jsx
@@ -7,19 +7,19 @@ import AchievementRow2 from './AchievementRow2'
 function Achievements() {
   return (
     <section className="h-full achievements flex flex-col cursor-default">
-        <div className="flex justify-between w-full h-[15%]" id="Achievements" >
+        <div className="flex justify-between w-full h-[150px]" id="Achievements" >
             <div>
-                <div className="hor-stripe1 my-5 mx-2 h-[8px] md:w-[200px] w-[100px] " data-aos="fade-right" data-aos-duration="800"></div>
-                <div className="ver-stripe1 mx-7 my-[-45px] w-[8px] md:h-[176px] h-[100px]"  data-aos="fade-up" data-aos-duration="800"></div>
+                <div className="hor-stripe1 mt-4 ml-2 h-[8px] md:w-[200px] w-[100px] " data-aos="fade-right" data-aos-duration="800"></div>
+                <div className="ver-stripe1 ml-6 -mt-5 w-[8px] md:h-[176px] h-[100px]"  data-aos="fade-up" data-aos-duration="800"></div>
             </div>
-            <div className="flex items-center gap-8 my-12 absolute left-[23%] sm:left-[29%]">
+            <div className="flex items-center justify-center gap-8 max-md:-translate-x-8">
                 <div><img src={trophy} alt="trophy" className="hidden lg:block w-20" /></div>
-                <div className="achi-head lg:text-6xl md:text-5xl text-xl" >Achievements</div>
+                <div className="achi-head lg:text-6xl md:text-5xl text-xl " >Achievements</div>
                 <div><img src={certificate} alt="certificate" className="w-20 hidden lg:block" /></div>
             </div>
             <div>
-                <div className="hor-stripe2 absolute top-5 right-3 h-[8px] md:w-[200px] w-[100px]" data-aos="fade-left" data-aos-duration="800"></div>
-                <div className="ver-stripe2 absolute top-1 right-7 w-[8px] md:h-[176px] h-[100px]"  data-aos="fade-up" data-aos-duration="800"></div>
+                <div className="hor-stripe2 mt-4 md:mr-2 -ml-16  h-[8px] md:w-[200px] w-[100px]" data-aos="fade-left" data-aos-duration="800"></div>
+                <div className="ver-stripe2 md:ml-28 ml-4 -mt-5 w-[8px] md:h-[176px] h-[100px]"  data-aos="fade-up" data-aos-duration="800"></div>
             </div>
         </div>
         <div className="achi-hero mt-28 flex flex-col gap-36 mb-28">
--- src/components/Documents.jsx
+++ src/components/Documents.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import DocumentCard from './DocumentCard'
 import UNlogo from './../assets/UNlogo.png'
 import Timeline from './../assets/timeline.jpg'
-import Partition from './../assets/partition.jpg'
+import Research from './../assets/Research.jpg'
 function Documents() {
   return (
     <div className="documents h-full" id="Documents">
@@ -12,7 +12,7 @@ function Documents() {
         <div className="w-full mt-14 mb-20 flex md:flex-row flex-col justify-around items-center">
             <DocumentCard Link={"https://drive.google.com/file/d/1Fikjl8qhAi2Dt67vtKRnwdQHqWz2uvOs/view?usp=drive_link"} Img={UNlogo} Heading={"General Study Guide"}/>
             <DocumentCard Link={"https://drive.google.com/file/d/1DySIKpVUddy3TFYQQhYd4w7JP1P47bvb/view?usp=drive_link"} Img={Timeline} Heading={"Timeline"}/>
-            <DocumentCard Link={"https://drive.google.com/file/d/1gu5N8duqmYiDWsQUZ3iwhInsdghC0iMK/view?usp=drive_link"} Img={Partition} Heading={"Background Guide - Partition of India"}/>
+            <DocumentCard Link={"https://drive.google.com/file/d/1BJ6HP2-1Y8yoYjLcQKwcjArpNLel00gH/view?usp=drive_link"} Img={Research} Heading={"Strategy and Research Guide"}/>
         </div>
     </div>
   )
