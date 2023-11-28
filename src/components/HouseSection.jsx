9c9bce69f6a6e15119a4ef70a76210d2271013b8
Divyam-1280 <divyamraj700@gmail.com>
2023-11-28 22:57:55 +0530

    stat changes

--- src/components/HouseSection.jsx
+++ src/components/HouseSection.jsx
@@ -11,12 +11,12 @@ function HouseSection() {
             House System
         </div>
         <div className="house-container h-full w-full mt-28 mb-12 lg:flex-row flex flex-col justify-around items-center gap-6">
-          <HouseCard Img={Gokhale} Title={"Gokhale House"} Des={"Embodying aggressive diplomacy, named in honor of the esteemed diplomat Vijay Shankar Gokhale. The Gokhale house strives for perfection"} Points={"To be updated"} id={"Gokhale"} />
-          <HouseCard Img={Jaishankar} Title={"Jaishankar House"} Des={"Inspired by diplomat Subrahmanyam Jaishankar, fostering diplomacy, global understanding, and effective communication."} Points={"To be updated"} id={"Jaishankar"} /> 
+          <HouseCard Img={Gokhale} Title={"Gokhale House"} Des={"Embodying aggressive diplomacy, named in honor of the esteemed diplomat Vijay Keshav Gokhale. The Gokhale house strives for perfection"} Points={"68"} id={"Gokhale"} />
+          <HouseCard Img={Jaishankar} Title={"Jaishankar House"} Des={"Inspired by diplomat Subrahmanyam Jaishankar, fostering diplomacy, global understanding, and effective communication."} Points={"84"} id={"Jaishankar"} /> 
         </div>
         <div className="house-container h-full w-full mb-12 lg:flex-row flex flex-col justify-around items-center gap-6">
-          <HouseCard Img={Bajpai} Title={"Bajpai House"} Des={"It is a tribute to Girija Shankar Bajpai, embodies diplomacy, grace and global awareness. Bajpai house has a legacy of excellence"} Points={"To be updated"} id={"Bajpai"}/>
-          <HouseCard Img={Menon} Title={"Menon House"} Des={"Named in honour of Mr. Shivashankar Menon. This house advocates for more peaceful and prosperous world through diplomacy"} Points={"To be updated"} id={"Menon"} /> 
+          <HouseCard Img={Bajpai} Title={"Bajpai House"} Des={"It is a tribute to Girija Shankar Bajpai, embodies diplomacy, grace and global awareness. Bajpai house has a legacy of excellence"} Points={"67"} id={"Bajpai"}/>
+          <HouseCard Img={Menon} Title={"Menon House"} Des={"Named in honour of Mr. Shivashankar Menon. This house advocates for more peaceful and prosperous world through diplomacy"} Points={"37"} id={"Menon"} /> 
         </div>
     </div>
   )
