7fdce619191963858884557118ec284dcbed7383
Divyam-1280 <divyamraj700@gmail.com>
2023-12-16 18:41:16 +0530

    content changes

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,26 +1,47 @@
-import React from 'react'
-import ScrollCarousel from 'scroll-carousel';
-import CarouselCard from './CarouselCard';
-import Mouni from './../assets/Mouni.jpg'
-import Chai from './../assets/Chai.jpg'
-import Varun from './../assets/varun.jpg'
-import Divyam from './../assets/divyam.jpg'
-import Priyanshu from './../assets/priyanshu.jpg'
-import Ananya from './../assets/ananya.jpg'
+import React from "react";
+import CarouselCard from "./CarouselCard";
+import Mouni from "./../assets/Mouni.jpg";
+import Chai from "./../assets/Chai.jpg";
+import Varun from "./../assets/varun.jpg";
+import Divyam from "./../assets/divyam.jpg";
+import Priyanshu from "./../assets/priyanshu.jpg";
+import Harish from "./../assets/harish2.jpg";
 
 function Carousel() {
   return (
     <div className="mt-40 mb-40 my-carousel overflow-x-scroll flex">
-        <CarouselCard Img={Mouni} Name={"Mouni Sanapala"} Position={"President"} />
-        <CarouselCard Img={Chai} Name={"Chaitanya Raj"} Position={"Secretary General"}/>
-        <CarouselCard Img={Varun} Name={"Varun Srivastava"} Position={"Chargé D' Affairs"}/>
-        <CarouselCard Img={Divyam} Name={"Divyam Raj Singh"} Position={"Under Secretary"}/>
-        <CarouselCard Img={Priyanshu} Name={"Priyanshu Das"} Position={"Under Secretary"}/>
-        <CarouselCard Img={Ananya} Name={"Ananya Shankar"} Position={"Treasurer"}/>
-        
+      <CarouselCard
+        Img={Mouni}
+        Name={"Mouni Sanapala"}
+        Position={"President"}
+      />
+      <CarouselCard
+        Img={Chai}
+        Name={"Chaitanya Raj"}
+        Position={"Secretary General"}
+      />
+      <CarouselCard
+        Img={Varun}
+        Name={"Varun Srivastava"}
+        Position={"Chargé D' Affairs"}
+      />
+      <CarouselCard
+        Img={Divyam}
+        Name={"Divyam Raj Singh"}
+        Position={"Under Secretary"}
+      />
+      <CarouselCard
+        Img={Priyanshu}
+        Name={"Priyanshu Das"}
+        Position={"Under Secretary"}
+      />
+      <CarouselCard
+        Img={Harish}
+        Name={"Harish Saharan"}
+        Position={"Treasurer"}
+      />
     </div>
-    
-  )
+  );
 }
 
-export default Carousel
+export default Carousel;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,76 +1,91 @@
-import React, { useState } from 'react'
-import logo from './../assets/MUNSOClogo.png'
-import HeaderItem from './../components/HeaderItem';
-import dots from './../assets/dots.png'
-import {HiDotsVertical} from "react-icons/hi";
-import 'aos/dist/aos.css';
-import Aos from 'aos';
-
+import React, { useState } from "react";
+import logo from "./../assets/MUNSOClogo.png";
+import HeaderItem from "./../components/HeaderItem";
+import dots from "./../assets/dots.png";
+import "aos/dist/aos.css";
+import Aos from "aos";
 
 function Header() {
   Aos.init();
 
   const [toggle, setToggle] = useState(false);
-  const menu=[
+  const menu = [
     {
-      name:'Home',
-      id:'',
+      name: "Home",
+      id: "",
     },
     {
-      name:'Achievements',
-      id: '#Achievements',
+      name: "Achievements",
+      id: "#Achievements",
     },
     {
-      name: 'Activity',
-      id: '#Activity',
+      name: "Activity",
+      id: "#Activity",
     },
     {
-      name: 'Documents',
-      id: '#Documents',
+      name: "Documents",
+      id: "#Documents",
     },
     {
-      name: 'About Us',
-      id: '#About',
+      name: "About Us",
+      id: "#About",
     },
     {
-      name: 'Contact',
-      id: '#Contact'
+      name: "Contact",
+      id: "#Contact",
     },
-  ]
+  ];
   return (
     <div className="flex justify-between items-center p-5 absolute z-10 top-0 w-full">
       <div className="flex items-center gap-8 md:ml-8">
+        <img
+          src={logo}
+          alt="logo"
+          className="w-[50px] md:w-[80px] object-cover"
+          data-aos="fade-right"
+          data-aos-duration="2000"
+        />
 
-      <img src={logo} alt="logo" className="w-[50px] md:w-[80px] object-cover" data-aos="fade-right" data-aos-duration="2000" />
-
-      <div className="hidden md:flex gap-8">
-      {menu.map((item)=>(
-        <a href={item.id}>
-        <HeaderItem name={item.name}/>
-        </a>
-      ))}
-      </div>
-      <div className="flex gap-3 md:hidden">
-      {menu.map((item,index)=>index<3&&(
-        <a href={item.id}>
-        <HeaderItem name={item.name}/>
-        </a>
-      ))}
-      <div className="md:hidden" onClick={()=>setToggle(!toggle)}>
-        <HeaderItem name={""}/>
-        <img src={dots} alt="dots" className=" mix-blend-multiply w-7 translate-y-[-10px] bg-transparent" />
-        {toggle? <div className="absolute mt-3 bg-black/60 rounded-md border-[1px] p-3 border-black/70 translate-x-[-85px]">
-        {menu.map((item,index)=>index>2&&(
-        <a href={item.id}>
-        <HeaderItem name={item.name}/>
-        </a>
-      ))}
-        </div>:null} 
-      </div>
-      </div>
+        <div className="hidden md:flex gap-8">
+          {menu.map((item) => (
+            <a href={item.id}>
+              <HeaderItem name={item.name} />
+            </a>
+          ))}
+        </div>
+        <div className="flex gap-3 md:hidden">
+          {menu.map(
+            (item, index) =>
+              index < 3 && (
+                <a href={item.id}>
+                  <HeaderItem name={item.name} />
+                </a>
+              )
+          )}
+          <div className="md:hidden" onClick={() => setToggle(!toggle)}>
+            <HeaderItem name={""} />
+            <img
+              src={dots}
+              alt="dots"
+              className=" mix-blend-multiply w-7 translate-y-[-10px] bg-transparent"
+            />
+            {toggle ? (
+              <div className="absolute mt-3 bg-black/60 rounded-md border-[1px] p-3 border-black/70 translate-x-[-85px]">
+                {menu.map(
+                  (item, index) =>
+                    index > 2 && (
+                      <a href={item.id}>
+                        <HeaderItem name={item.name} />
+                      </a>
+                    )
+                )}
+              </div>
+            ) : null}
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
