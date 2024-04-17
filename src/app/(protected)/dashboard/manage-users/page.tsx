e8e138cae94885743556fee6ec05aaf70eddb4a5
Shivam Shekhar Soy <shivamshekharsoy@gmail.com>
2024-04-17 14:12:33 +0530

    feat: add basic user management

---
+++ src/app/(protected)/dashboard/manage-users/page.tsx
@@ -0,0 +1,24 @@
+import UserCard from "@/components/dashboard/user-card"
+import { getUserList } from "@/server/actions/userActions"
+import { getUserAuth } from "@/server/auth/utils"
+
+export default async function Page() {
+  const userList = await getUserList()
+  const { session } = await getUserAuth()
+  return (
+    <>
+      <div className="space-y-4 p-4 sm:pt-8 w-full max-w-7xl">
+        <div className="space-y-2">
+          <div className="flex justify-between">
+            <h1 className="text-2xl font-bold">Change User Roles</h1>
+          </div>
+          <ul className="w-full grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+            {userList.filter((user) => user.id !== session?.user.id).map((user) => (
+              <UserCard key={user.id} user={user} />
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
+  )
+}
