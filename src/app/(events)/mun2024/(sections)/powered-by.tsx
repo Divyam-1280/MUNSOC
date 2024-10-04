df75bdb014c25574cce04a7b83bf39e194debfd0
Shivam Shekhar Soy <shivamshekharsoy@gmail.com>
2024-10-04 12:48:42 +0530

    sponsors

--- src/app/(events)/mun2024/(sections)/powered-by.tsx
+++ src/app/(events)/mun2024/(sections)/powered-by.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image"
 import UnstopBlue from "../assets/Unstop-Logo-Blue-Medium.png"
+import Unistyl from "../assets/White.png"
 
 export default function PoweredBy() {
   return (
     <section className="py-14 sm:pt-20 max-w-[1370px] w-full mx-auto max-lg:px-6">
-      <h1 className="text-3xl sm:text-6xl tracking-tighter text-nowrap">Powered by</h1>
-      <div className="grid grid-rows-1 w-full mt-8 gap-6">
-        <Image src={UnstopBlue} alt="unstop" width={640} height={360} className="h-36 w-auto place-self-center" />
+      <h1 className="text-3xl sm:text-6xl tracking-tighter text-nowrap">Sponsors</h1>
+      <div className="flex flex-wrap justify-center items-center w-full mt-8 gap-8">
+        <Image src={UnstopBlue} alt="unstop" width={640} height={360} className="h-28 w-auto place-self-center" />
+        <Image src={Unistyl} alt="unistyl" width={640} height={360} className="max-sm:h-20 h-28 w-auto place-self-center" />
       </div>
     </section>
   )
